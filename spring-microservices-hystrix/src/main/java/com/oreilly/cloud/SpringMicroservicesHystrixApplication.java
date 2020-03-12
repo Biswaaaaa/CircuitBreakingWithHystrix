d5dc36f70b70f267148e793d9110fbd06cd7a4e1
Biswaaaaa <ibbarman@gmail.com>
2020-03-12 15:15:26 +0530

    Adding New Project and changing the properties

---
+++ spring-microservices-hystrix/src/main/java/com/oreilly/cloud/SpringMicroservicesHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.oreilly.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesHystrixApplication.class, args);
+	}
+
+}
